80968dd82fb98d23a6d811ef409edbb095063cc5
mohammadsajal <54902952+mohammadsajal@users.noreply.github.com>
2019-11-18 13:21:13 -0600

    Add files via upload

---
+++ firstExercise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Celsius
+{
+    class firstExercise
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+
+            Console.Write("Enter Fahrenheit Temp : ");
+
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine("The converted Celsius Temp is " + celsius);
+
+            Console.ReadLine();
+        }
+    }
+}
