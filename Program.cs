fe0e9d5178da69f69c99f1d9f41dae757754493d
mohammadsajal <54902952+mohammadsajal@users.noreply.github.com>
2019-12-04 08:52:03 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,53 @@
 ﻿using System;
 
+namespace lab_03_SAJAL
+	{
+		class Program
+		{
 
-namespace Lab02
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double pair_of_single_prescriiption_glasses_cost = 40.00;
-            double non_prescriptio_sunglasses_cost = 25.00;
-            double anti_glare_cost = 12.50;
-            double brown_tint_coating_cost = 9.99;
-            Console.WriteLine("What kind of glasses would you like: ");
-            Console.WriteLine("1 -> prescription, 2 -> non-prescription : ");
-            int customer_selection = int.Parse(Console.ReadLine());
-            while (customer_selection < 1 || customer_selection > 2)
-            {
-                Console.WriteLine("1 -> prescription, 2 -> non-prescription : ");
-                customer_selection = int.Parse(Console.ReadLine());
-            }
-            double total_cost = 0.0;
-            if (customer_selection == 1)
-                total_cost = pair_of_single_prescriiption_glasses_cost;
-            else
-                total_cost = non_prescriptio_sunglasses_cost;
-            Console.WriteLine("What kind of coating would you like: ");
-            Console.WriteLine("1 -> anti-glare, 2 -> brown tint : ");
-            customer_selection = int.Parse(Console.ReadLine());
-            while (customer_selection < 1 || customer_selection > 2)
-            {
-                Console.WriteLine("1 -> anti-glare, 2 -> brown tint : ");
-                customer_selection = int.Parse(Console.ReadLine());
+			public static double validate(double bal, double amount)
+			{
+				//deposit
+				if (amount > 0)
+				{
+					return bal + amount;
+				}
 
-            }
-            if (customer_selection == 1)
-                total_cost += anti_glare_cost;
-            else
-                total_cost += brown_tint_coating_cost;
-            Console.WriteLine(" Your total cost is $" + total_cost);
-            Console.ReadLine();
-        }
-    }
-}
+				//withdraw
+				if (bal < amount)
+				{
+					return bal;
+				}
+				return bal - amount;
+			}
+
+			public static void Main(string[] args)
+			{
+
+				double curBal = 45.32;
+
+				double amount;
+				Console.Write("Please enter a amount to update account by $");
+				amount = Int32.Parse(Console.ReadLine());
+				Console.Write("\n");
+
+				Console.Write("Customer\'s balance (before transaction) = $");
+				Console.WriteLine(curBal + "\n");
+				Console.Write("Requested update amount = $");
+				Console.WriteLine(amount + "\n");
+
+				double actAmount;
+				actAmount = validate(curBal, amount);
+				curBal += actAmount;
+
+				Console.Write("Actual update amount = $");
+				Console.WriteLine(actAmount + "\n");
+				Console.Write("Customer\'s balance (after transaction) = $");
+				Console.WriteLine(curBal + "\n");
+
+				Console.WriteLine("\nThank you and good-bye!\n");
+			}
+
+		}
+	}
+
